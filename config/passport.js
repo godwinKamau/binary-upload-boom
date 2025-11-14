0118dae4f7cee211368932594c41943b6c4d0cb9
Godwin Kamau <contactgodwinkamau@gmail.com>
2025-11-14 14:49:11 -0500

    started fixing auth

--- config/passport.js
+++ config/passport.js
@@ -4,11 +4,10 @@ const User = require("../models/User");
 
 module.exports = function (passport) {
   passport.use(
-    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
-      User.findOne({ email: email.toLowerCase() }, (err, user) => {
-        if (err) {
-          return done(err);
-        }
+    new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
+      await User.findOne({ email: email.toLowerCase() })
+      .then(user => {
+        
         if (!user) {
           return done(null, false, { msg: `Email ${email} not found.` });
         }
@@ -27,7 +26,12 @@ module.exports = function (passport) {
           }
           return done(null, false, { msg: "Invalid email or password." });
         });
-      });
+      })
+      .catch(err => {
+        if (err) {
+          return done(err);
+        }
+      })
     })
   );
 
